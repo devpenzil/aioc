9f217c6e462a57d67bc05361d4f3ef933d314159
Mahesh <mahesh.sk@neoito.com>
2023-10-29 18:00:17 +0530

    release fix
    '

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Profile from "./pages/Profile";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
